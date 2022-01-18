fe1fa044a0355d5a98a5d2c584c502bb5982e6e0
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-18 05:39:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Write a python program to read the sides ofa triangle a, b and calculate the hypotenue using the formula square root(square of a + square of b) 
+import math
+a = int(input())
+b = int(input())
+h=0
+
+print(math.sqrt((a*a)+(b*b)))
